a0b0f6a2da8a59d6e1d4252fc4fb46aa53ef5dbf
liujiani505 <64924326+liujiani505@users.noreply.github.com>
2022-02-08 13:33:35 -0500

    finished contact form

--- pages/contact.js
+++ pages/contact.js
@@ -1,10 +1,20 @@
-import { Container, Heading, Button, Input, FormLabel, Textarea } from "@chakra-ui/react";
+import { Container, Box, Heading, Button, Input, Textarea, FormControl, FormLabel, Alert, AlertIcon} from "@chakra-ui/react";
 import Section from '../components/section'
-// import { HiOutlineMailOpen } from "react-icons/hi"
-// import { FaLinkedinIn, FaGithub } from "react-icons/fa"
+import { useState } from "react";
 
 
 const Contact = () => {
+
+    const [newForm, setNewForm] = useState({
+        name: "",
+        email: "",
+        message: "",
+    })
+    const [message, setMessage] = useState('');
+
+    const handleChange = (e) => {
+        setNewForm({...newForm, [e.target.name]: e.target.value})
+    }
 
     async function handleOnSubmit(e){
         e.preventDefault();
@@ -17,26 +27,36 @@ const Contact = () => {
             method: 'post',
             body: JSON.stringify(formData)
         })
-        console.log(formData)
+        setMessage(`Thank you for your message! I'll get back to you as soon as possible.`)
+        setNewForm({
+            name:"",
+            email:"",
+            message: "",
+        })
     }
 
     return (
         <Container mt={10} maxW="container.lg">
+            
                 <Section delay={0.1}>
                     <Heading as="h3" fontSize={35} mb={4}>Contact Me</Heading>
                     <p>To inquire about collaborations or potential projects, get in touch with me!</p>
                 </Section>
                 <Section delay={0.2}>
                     <form method ="post" onSubmit={handleOnSubmit}>
+                        <FormControl isRequired>
                             <FormLabel htmlFor='name'>Name</FormLabel>
-                            <Input name='name' type='text' placeholder='Enter your name'/>
+                            <Input name='name' type='text' placeholder='Enter your name' value={newForm.name} onChange={handleChange}/>
                             <FormLabel htmlFor='email' mt={3}>Email</FormLabel>
-                            <Input name='email' type='email' placeholder='Enter your email'/>
+                            <Input name='email' type='email' placeholder='Enter your email' value={newForm.email} onChange={handleChange}/>
                             <FormLabel htmlFor='message' mt={3}>Message</FormLabel>
-                            <Textarea name='message' placeholder='Enter your message'/>
-                        <Button type="submit" color='#000000' bg="#CBD5E0" size='md' mt={6}>Submit</Button>
+                            <Textarea name='message' placeholder='Enter your message' value={newForm.message} onChange={handleChange}/>                       
+                        </FormControl>
+                        <Button type="submit" color='#000000' bg="#CBD5E0" size='md' mt={6}>Submit</Button><br/>
                     </form>
+                <Box mt={5} color="#38B2AC">{message}</Box>
                 </Section>
+
         </Container>
     )
 }
